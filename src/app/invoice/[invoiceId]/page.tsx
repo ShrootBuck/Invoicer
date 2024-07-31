cbf04828ff512f506f20d7c2a66e1ff4b586019b
ShrootBuck <ShrootBuck@users.noreply.github.com>
2024-07-31 09:07:40 -0700

    Fix up some styling

--- src/app/invoice/[invoiceId]/page.tsx
+++ src/app/invoice/[invoiceId]/page.tsx
@@ -5,13 +5,23 @@ export default async function InvoicePage({
 }: {
   params: { invoiceId: string };
 }) {
-  const invoice = await db.invoice.findUnique({
+  const invoice = await db.invoice.findFirst({
     where: { id: parseInt(params.invoiceId) },
+    include: { logs: true },
   });
 
   if (!invoice) {
-    return <p>invoice not found</p>;
+    return <h1 className="text-center text-xl">Invoice not found!</h1>;
   }
+
+  await db.timeLog.create({
+    data: {
+      startTime: new Date(),
+      invoice: {
+        connect: { id: invoice.id },
+      },
+    },
+  });
 
   return (
     <div>
@@ -19,7 +29,14 @@ export default async function InvoicePage({
       <h2 className="text-center text-lg text-gray-400/75">
         {invoice.description}
       </h2>
-      <h3>Last Updated: {invoice.updatedAt.toLocaleString()}</h3>
+      <h3 className="text-center">
+        Last Updated: {invoice.updatedAt.toLocaleString()}
+      </h3>
+      <h4>
+        {invoice.logs.map((log) => {
+          return log.endTime.toLocaleString();
+        })}
+      </h4>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,41 +12,55 @@ import { db } from "~/server/db";
 
 export default async function Home() {
   const session = await getServerAuthSession();
+
   const invoices = await db.invoice.findMany({
     where: { createdById: session?.user.id },
   });
 
   return (
-    <div className="grid grid-cols-1 p-5 md:grid-cols-4">
-      {invoices ? (
-        invoices.map((invoice: Invoice) => (
-          <Card className="m-5 max-w-[400px]" key={invoice.id}>
-            <CardHeader className="flex gap-3">
-              <div className="flex flex-col">
-                <p className="text-md">{invoice.name}</p>
-                <p className="text-small text-default-500">
-                  Created on {invoice.createdAt.toLocaleDateString()}
-                </p>
-              </div>
-            </CardHeader>
-            <Divider />
-            <CardBody>
-              <p>
-                {invoice.description
-                  ? invoice.description
-                  : "No description provided."}
-              </p>
-            </CardBody>
-            <Divider />
-            <CardFooter>
-              <Link isExternal showAnchorIcon href={`/invoice/${invoice.id}`}>
-                Open Invoice
-              </Link>
-            </CardFooter>
-          </Card>
-        ))
+    <div>
+      {invoices.length > 0 ? (
+        <>
+          <div className="grid grid-cols-1 p-5 md:grid-cols-4">
+            {invoices.map((invoice: Invoice) => (
+              <Card className="m-5 max-w-[400px]" key={invoice.id}>
+                <CardHeader className="flex gap-3">
+                  <div className="flex flex-col">
+                    <p className="text-md">{invoice.name}</p>
+                    <p className="text-small text-default-500">
+                      Created on {invoice.createdAt.toLocaleDateString()}
+                    </p>
+                  </div>
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                  <p>
+                    {invoice.description
+                      ? invoice.description
+                      : "No description provided."}
+                  </p>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                  <Link
+                    isExternal
+                    showAnchorIcon
+                    href={`/invoice/${invoice.id}`}
+                  >
+                    Open Invoice
+                  </Link>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
+        </>
       ) : (
-        <span>lol</span>
+        <div className="flex items-center justify-center space-x-2">
+          <h1 className="text-xl">No invoices found!</h1>
+          <Link className="text-xl" href="/create-invoice">
+            Create One.
+          </Link>
+        </div>
       )}
     </div>
   );
